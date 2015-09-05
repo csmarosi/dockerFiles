263454dedba74b01abd7afd4ea890fe0dcf641bf
Csaba Marosi <cs.marosi@gmail.com>
2015-09-05 17:25:19 +0000

    Run arbitrary commands with bin/run

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ if echo ${cmdArg} | grep -q 'pdf$'; then
     okular /magic/$(basename ${cmdArg})
 elif echo ${cmdArg} | grep -q 'callgrind'; then
     kcachegrind /magic/$(basename ${cmdArg})
+elif [ -n "${1}" ]; then
+    cd ${HOME}
+    ${@}
 else
     bash
 fi
