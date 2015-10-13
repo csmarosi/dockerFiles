935a188b251219ae307dc51aed35205123743387
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2015-10-13 12:34:04 +0000

    reBuildAll.sh: consider own base images

--- reBuildAll.sh
+++ reBuildAll.sh
@@ -2,10 +2,18 @@
 set -x
 set -e
 
-for image in $(git grep '[F]ROM' | cut -d' ' -f2 | sort | uniq); do
+baseImages=$(git grep '[F]ROM' | cut -d' ' -f2 | sort | uniq)
+imagesToBuild=$(echo ${baseImages} | tr ' ' '\n' | grep  -E $(git ls-files | awk -F/ '/Dockerfile/{print "^"$1"$"}' | tr '\n' '|')GrepFiller)
+imagesToPull=$( echo ${baseImages} | tr ' ' '\n' | grep -vE $(git ls-files | awk -F/ '/Dockerfile/{print "^"$1"$"}' | tr '\n' '|')GrepFiller)
+
+for image in ${imagesToPull}; do
     docker pull ${image}
 done
 
+for myImage in ${imagesToBuild}; do
+    ./build.sh ${myImage}
+done
+
 for myImage in $(git ls-files | awk -F/ '/Dockerfile/ {print $1}'); do
-    ./build.sh --no-cache ${myImage}
+    ./build.sh ${myImage} || true
 done
