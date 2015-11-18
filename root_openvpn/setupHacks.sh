1aea972066fd7e23a6ff8cecfa18222732859117
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2015-11-18 07:39:50 +0000

    Added openvpn container to provide VPN tunnel to other containers

---
+++ root_openvpn/setupHacks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+set -e
+
+configFile=/magic/client-config.ovpn
+test -f ${configFile}
+
+remoteIp=$(awk '/^remote/{print $2}' ${configFile})
+currentRoute=$(ip route show ${remoteIp}/0 | cut -d' ' -f3)
+ip route add ${remoteIp} via ${currentRoute}
+
+openvpn ${configFile} &
+while ! ip addr | grep -Eq 'inet .*(tun|tap)'; do
+    sleep 1
+done
+
+newRoute=$(ip route | grep -E 'tun|tap' | awk '/via/{print $3}' | head -n1)
+ip route change default via ${newRoute}
+
+exec sleep infinity
