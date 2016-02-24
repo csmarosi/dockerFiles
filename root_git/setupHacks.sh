ec52bdf2772e93c850d2642b36e332ebdd7cf444
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2016-02-24 18:10:27 +0000

    Misc enhancements

--- root_git/setupHacks.sh
+++ root_git/setupHacks.sh
@@ -4,5 +4,8 @@ set -e
 
 /etc/init.d/ssh start
 
+echo "3 5 * * * root /backup.sh" > /etc/cron.d/backup
+/etc/init.d/cron start
+
 #no exec to reap the zombies
 sleep infinity
--- root_tor/setupHacks.sh
+++ root_tor/setupHacks.sh
@@ -19,10 +19,13 @@ iptables -F
 iptables -t nat -F
 _tor_uid=$(awk -F: '/^debian-tor:/{print $3}' /etc/passwd)
 iptables -t nat -A OUTPUT -m owner --uid-owner $_tor_uid -j RETURN
+iptables -t nat -A OUTPUT -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A OUTPUT -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports 9040
 iptables -A OUTPUT -m owner --uid-owner $_tor_uid -j ACCEPT
 iptables -A OUTPUT -d 127.0.0.0/8 -j ACCEPT
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -d 192.168.0.0/16 -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -j REJECT
 
 /usr/sbin/tor -f /etc/torrc
