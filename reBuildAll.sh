3ae47f11976404138b4874db1dac15eb9c38b979
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2016-03-05 15:05:47 +0000

    Various changes

--- reBuildAll.sh
+++ reBuildAll.sh
@@ -3,21 +3,28 @@ set -x
 set -e
 
 baseImages=$(git grep '[F]ROM' | cut -d' ' -f2 | sort | uniq)
-imagesToBuild=$(echo ${baseImages} | tr ' ' '\n' | grep  -E $(git ls-files | awk -F/ '/Dockerfile/{print "^"$1"$"}' | tr '\n' '|')GrepFiller)
+imagesToBuild=$(echo ${baseImages} | tr ' ' '\n' | grep  -E $(git ls-files | awk -F/ '/Dockerfile/{print "^"$1"$"}' | tr '\n' '|')GrepFiller || true)
 imagesToPull=$( echo ${baseImages} | tr ' ' '\n' | grep -vE $(git ls-files | awk -F/ '/Dockerfile/{print "^"$1"$"}' | tr '\n' '|')GrepFiller)
 
+createMissing=false
+echo ${@} | grep -q -- --all && createMissing=true
+pullOnly=true
+echo ${@} | grep -q -- --build && pullOnly=false
+
+existingImages="$(docker images | awk '{print $1":"$2}')"
 for image in ${imagesToPull}; do
+    echo "${existingImages}" | grep -q ${image} || ${createMissing} && \
     docker pull ${image}
 done
 
 for myImage in ${imagesToBuild}; do
+    echo "${existingImages}" | grep -q ${myImage} || ${createMissing} && \
     ./build.sh ${myImage}
 done
 
-if [ "${1}" != '--all' ]; then
-    exit 0
-fi
+${pullOnly} && exit 0
 
 for myImage in $(git ls-files | awk -F/ '/Dockerfile/ {print $1}'); do
+    echo "${existingImages}" | grep -q ${myImage} || ${createMissing} && \
     ./build.sh ${myImage} || true
 done
