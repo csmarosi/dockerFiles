baec4617977afc7d63e4a5a976dd6c04032cd13b
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2016-12-28 17:14:35 +0000

    dnscrypt contains a DNS cacher

---
+++ root_dnscrypt/setupHacks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+set -e
+
+counter=1
+for dnsServer in ${@}; do
+    counter=$((${counter} + 1))
+    listenIp="127.0.0.${counter}"
+    echo nameserver ${listenIp} >> /etc/resolv.conf.all
+    /usr/sbin/dnscrypt-proxy -R ${dnsServer} -a ${listenIp}:53 &
+done
+
+shuf -n3 /etc/resolv.conf.all > /etc/resolv.conf
+cat /etc/resolv.conf
+exec /usr/sbin/dnsmasq
