9343d79f47b162103da71d98c43ba4ba2ea8e6c7
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2016-12-28 17:14:53 +0000

    Added standalone RDP screen with xmonad

--- chromium_gui/entrypoint.sh
+++ chromium_gui/entrypoint.sh
@@ -2,26 +2,9 @@
 set -x
 set -e
 
-cd ${HOME}
 profileGenerator=${HOME}/browser_profiles/chromiumPrivate.py
-if [ -f ${profileGenerator} ]; then
-    cp .ratpoisonrc.common .ratpoisonrc
-else
+if [ ! -f ${profileGenerator} ]; then
     profileGenerator=/tmp/chromium.py
 fi
 
-cat > /tmp/start.sh <<EOF
-export PULSE_SERVER=${PULSE_SERVER}
-export dockerImageName=${dockerImageName}
 exec ${profileGenerator}
-EOF
-
-if [[ "${DISPLAY}" == ":0" ]]; then
-    exec bash /tmp/start.sh
-else
-    #A password is needed, but on local network does not really matter what
-    echo $(whoami):$(whoami) | sudo chpasswd
-    echo 'exec bash /tmp/start.sh' >> .ratpoisonrc
-    sudo /etc/init.d/xrdp.sh start
-    exec sleep infinity
-fi
---
+++ xrdp_xmonad/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+set -e
+
+cd ${HOME}
+shopt -s dotglob
+#/tmp and ${HOME} is bind mounted from a permanent place
+sudo rm -rf /tmp/*
+
+echo $(whoami):$(whoami) | sudo chpasswd
+sudo /etc/init.d/xrdp.sh start
+
+#test -S /tmp/.X11-unix/X0
+sleep infinity
