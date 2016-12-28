96e7ec859860ba26a0e0b40fa7dac62b8128f470
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2016-12-28 17:15:02 +0000

    Added packages

--- root_tor/setupHacks.sh
+++ root_tor/setupHacks.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-cat > /etc/torrc <<EOF
+test -f /etc/torrc || cat > /etc/torrc <<EOF
 VirtualAddrNetworkIPv4 10.192.0.0/10
 AutomapHostsOnResolve 1
 TransPort 9040
@@ -24,13 +24,13 @@ EOF
 iptables -F
 iptables -t nat -F
 _tor_uid=$(awk -F: '/^debian-tor:/{print $3}' /etc/passwd)
+iptables -t nat -A OUTPUT -o tun+ -j RETURN
 iptables -t nat -A OUTPUT -m owner --uid-owner $_tor_uid -j RETURN
 iptables -t nat -A OUTPUT -d 127.0.0.0/8 -j RETURN
-iptables -t nat -A OUTPUT -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports 9040
+iptables -A OUTPUT -o tun+ -j ACCEPT
 iptables -A OUTPUT -m owner --uid-owner $_tor_uid -j ACCEPT
 iptables -A OUTPUT -d 127.0.0.0/8 -j ACCEPT
-iptables -A OUTPUT -d 192.168.0.0/16 -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -j REJECT
 
