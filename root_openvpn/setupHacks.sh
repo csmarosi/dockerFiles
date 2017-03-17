e0921f151bc9d63498c91ce7939827558bce00f3
Csaba Marosi <4bea6c869366227b879ffe4abad50c@gmail.com>
2017-03-17 05:44:52 +0000

    Added random fixes, enhancements, and container

--- root_openvpn/setupHacks.sh
+++ root_openvpn/setupHacks.sh
@@ -2,13 +2,15 @@
 set -x
 set -e
 
-configFile=/magic/client-config.ovpn
+configFile=/magic/${1}
 test -f ${configFile}
 
-remoteIp=$(awk '/^remote/{print $2}' ${configFile})
-currentRoute=$(ip route show ${remoteIp}/0 | cut -d' ' -f3)
-ip route add ${remoteIp} via ${currentRoute}
-ip route add 192.168.0.0/16 via ${currentRoute}
+remoteIp=$(awk '/^remote /{print $2}' ${configFile})
+currentRoute=$(ip route get ${remoteIp}/32 | head -n1 | cut -d' ' -f3)
+ip route | grep -q ${remoteIp} || \
+    ip route add ${remoteIp} via ${currentRoute}
+ip route | grep -q 192.168.0.0/16 || \
+    ip route add 192.168.0.0/16 via ${currentRoute}
 
 openvpn ${configFile} &
 while ! ip addr | grep -Eq 'inet .*(tun|tap)'; do
